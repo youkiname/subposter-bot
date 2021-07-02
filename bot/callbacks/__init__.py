01b788d2648b13f068cf268edc33948224d97da7
youkiname <youkiname@mail.ru>
2021-07-02 14:31:37 +0300

    add new exception handler

--- bot/callbacks/__init__.py
+++ bot/callbacks/__init__.py
@@ -1,3 +1,8 @@
+from telebot.types import CallbackQuery
+from bot import bot
+from config import config
+import logging
+
 
 class CallbackCommands:
     POST_LIKE = '0'
@@ -6,4 +11,13 @@ class CallbackCommands:
     SET_SHOW_NAME = '3'
 
 
-
+def callback_exception_handler(handler):
+    def wrapper_accepting_arguments(callback: CallbackQuery):
+        try:
+            handler(callback)
+        except Exception as e:
+            bot.answer_callback_query(callback.id, "Произошла ошибка: {}.".format(type(e).__name__), show_alert=True)
+            bot.send_message(config.EXCEPTION_LOG_CHAT_ID, "Поймано исключение {} в {}:\n"
+                                                           "{}".format(type(e).__name__, handler.__name__, e))
+            logging.exception(e)
+    return wrapper_accepting_arguments
